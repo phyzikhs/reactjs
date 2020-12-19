7e4f099e8c19f71f7ef9273a724a75e40b40bc47
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-19 17:34:28 +0200

    UI components don't need to be a class Make a constance function

--- react-redux/myapp/src/Ninjas.js
+++ react-redux/myapp/src/Ninjas.js
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
 
-class Ninjas extends Component{
-  render() {
-    console.log(this.props);
-    const {ninjas} = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.key}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-      </div>
-      )
-    })
+const Ninjas = ({ninjas}) => {
+  console.log(ninjas);
+  const ninjaList = ninjas.map(ninja => {
     return (
-      <div className="ninja-list">
-        {ninjaList}
-      </div>
+      <div className="ninja" key={ninja.key}>
+      <div>Name: {ninja.name}</div>
+      <div>Age: {ninja.age}</div>
+      <div>Belt: {ninja.belt}</div>
+    </div>
     )
-  }
+  });
+
+  return (
+    <div className="ninja-list">
+      {ninjaList}
+    </div>
+  )
 }
 
 export default Ninjas;
