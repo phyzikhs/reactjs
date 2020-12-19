e1696fae92defd836a3d7af008abc33822d6527d
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-19 16:50:36 +0200

    "npx create-react-app myapp cd myapp npm start" Created a new Ninja component. Components are referenced as HTML tags

---
+++ react-redux/myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Ninjas from "./Ninjas"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My first react app</h1>
+        <p>Welcome :-)</p>
+        <Ninjas/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
