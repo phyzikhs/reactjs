767ab6be3db77bed4da2e1dfa3895c7a449400bc
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-19 17:22:00 +0200

    list of objects and their id as a key use the map function to map elements this.state andt state is tripping

--- react-redux/myapp/src/App.js
+++ react-redux/myapp/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import Ninjas from "./Ninjas"
 
 function App() {
+  const state = {
+    ninjas:[
+      {name: "Scelo", age: 22, belt: "Black", key: 1},
+      {name: "Zakes", age: 21, belt: "Blue", key: 2},
+      {name: "Taz", age: 20, belt: "Brown", key: 3},
+    ],
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>My first react app</h1>
         <p>Welcome :-)</p>
-        <Ninjas name="Sicela" age="21" belt="Blue"/>
-        <Ninjas name="Zakes" age="22" belt="Black"/>
+        <Ninjas ninjas={state.ninjas}/>
       </header>
     </div>
   );
