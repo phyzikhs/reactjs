1852dde18f774aa4914d5cd24f4c7dbbdf1654df
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-19 16:59:41 +0200

    Props in the tag can be dynamic but musltiple incoming tags is still tideous

--- react-redux/myapp/src/App.js
+++ react-redux/myapp/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <h1>My first react app</h1>
         <p>Welcome :-)</p>
-        <Ninjas/>
+        <Ninjas name="Sicela" age="21" belt="Blue"/>
+        <Ninjas name="Zakes" age="22" belt="Black"/>
       </header>
     </div>
   );
