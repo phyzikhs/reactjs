f889f2d02d55f3be95c643a3655c7bd4619d620c
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-20 18:28:55 +0200

    Added a new ninja component as a class App.js changed from a function into a class, accommodating states also added an addNinja method Remember that ninjas are filtered according to age Also added an AddNinja tag passing an addNinja function as props function makes things complicated if using state Changed key on ninjas into id as in tut vidoe

--- react-redux/myapp/src/App.js
+++ react-redux/myapp/src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
+import AddNinja from "./AddNinja";
 import Ninjas from "./Ninjas"
 
-function App() {
-  const state = {
+class App extends Component {
+  state = {
     ninjas:[
-      {name: "Scelo", age: 22, belt: "Black", key: 1},
-      {name: "Zakes", age: 21, belt: "Blue", key: 2},
-      {name: "Taz", age: 20, belt: "Brown", key: 3},
+      {name: "Scelo", age: 22, belt: "Black", id: 1},
+      {name: "Zakes", age: 21, belt: "Blue", id: 2},
+      {name: "Taz", age: 20, belt: "Brown", id: 3},
     ],
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My first react app</h1>
-        <p>Welcome :-)</p>
-        <Ninjas ninjas={state.ninjas}/>
-      </header>
-    </div>
-  );
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas  = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My first react app</h1>
+          <p>Welcome :-)</p>
+          <Ninjas ninjas={this.state.ninjas}/>
+          <AddNinja addNinja= {this.addNinja}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-redux/myapp/src/Ninjas.js
+++ react-redux/myapp/src/Ninjas.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 const Ninjas = ({ninjas}) => {
-  console.log(ninjas);
   const ninjaList = ninjas.map(ninja => {
     return ninja.age<=20 ? null : (
-      <div className="ninja" key={ninja.key}>
+      <div className="ninja" key={ninja.id}>
       <div>Name: {ninja.name}</div>
       <div>Age: {ninja.age}</div>
       <div>Belt: {ninja.belt}</div>
