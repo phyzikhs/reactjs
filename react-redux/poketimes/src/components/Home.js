f6860aa73c962e7634eff3fec3c1fa5a7ac93095
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-22 00:26:13 +0200

    Got a post using Axios on Post.js assign the given post from result to the state store the html of the post in the post constance render the post use ternary operator to check o=if post is null

--- react-redux/poketimes/src/components/Home.js
+++ react-redux/poketimes/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 class Home extends Component {
@@ -21,7 +22,9 @@ class Home extends Component {
 				return(
 					<div className="post card" key={post.id}>
 						<div className="card-content">
-							<span className="card-title">{post.title}</span>
+							<Link to={"/"+post.id}>
+								<span className="card-title">{post.title}</span>
+							</Link>
 							<p>{post.body}</p>
 						</div>
 					</div>
--- react-redux/poketimes/src/components/Post.js
+++ react-redux/poketimes/src/components/Post.js
@@ -1,20 +1,33 @@
+import Axios from 'axios';
 import React, { Component } from 'react'
 
 class Post extends Component{
     state = {
-        id: null
+        post: null
     }
     componentDidMount(){
         // console.log(this.props);
         let id = this.props.match.params.post_id;
-        this.setState({
-            id: id
-        })
+        Axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
+        .then(res => {
+            this.setState({
+                post: res.data
+            })
+            console.log(res);
+        });
       }
     render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h2 className="center">{this.state.post.title}</h2>
+                <p>{this.state.post.body}</p>
+            </div>
+        ) : (
+            <div className="center">Loading post...</div>
+        );
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4>{post}</h4>
             </div>
         )
     }
