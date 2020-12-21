c3aeb47657737bbdb6e14ada9675f4776045a62b
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-22 00:33:11 +0200

    Used a switch tag to stop all routes when one matches The order of routers matters now in the App.js

--- react-redux/poketimes/src/App.js
+++ react-redux/poketimes/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom' //as Router, Route, NavLink
+import { BrowserRouter, Route, Switch } from 'react-router-dom' //as Router, Route, NavLink
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -12,10 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
-          <Route exact path="/:post_id" component={Post}/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+            <Route exact path="/:post_id" component={Post}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
