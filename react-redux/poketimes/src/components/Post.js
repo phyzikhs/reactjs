c796a336fabaae29957880347f231fe25de14e59
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-22 22:50:15 +0200

    rootReducer: added a condition for the DELETE_POST action it uses the filter method
    Post:
    created a delete button and assigned a deletePost method
    deletePost method added as props
    mapDispatchToProps passes dispatch function
    deletePost just gives rootReducer the id and acrion

--- react-redux/poketimes/src/components/Post.js
+++ react-redux/poketimes/src/components/Post.js
@@ -2,12 +2,19 @@ import {connect} from 'react-redux'
 import React, { Component } from 'react'
 
 class Post extends Component{
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
+    }
     render() {
         console.log(this.props);
         const post = this.props.post ? (
             <div className="post">
                 <h2 className="center">{this.props.post.title}</h2>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>Delete Post</button>
+                </div>
             </div>
         ) : (
             <div className="center">Loading post...</div>
@@ -26,4 +33,12 @@ const mapStateToProps = (state, ownProps) => {
         post: state.posts.find(post => post.id===id)
     }
 }
-export default connect(mapStateToProps)(Post);
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => {
+            dispatch({type: 'DELETE_POST', id: id}); //dispatchEvent
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
--- react-redux/poketimes/src/reducers/rootReducer.js
+++ react-redux/poketimes/src/reducers/rootReducer.js
@@ -8,7 +8,16 @@ const initState = {
 	]
 }
 const rootReducer = (state = initState, action) => {
-		return state;
+	if(action.type === 'DELETE_POST'){
+		let newPosts = state.posts.filter(post => {
+			return post.id != action.id;
+		});
+		return {
+			...state,
+			posts: newPosts
+		}
+	}
+	return state;
 }
 
 export default rootReducer;
