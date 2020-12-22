f6a2f65677659a13959433dbdb47bdc53dd6e416
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-23 00:46:12 +0200

    Dashboard: Nodifications and ProjectList Added dummy data for project list imported Switch and Route for App.js routing iported Dashboard into app and added its home route

--- react-redux-firebase/marioplan/src/App.js
+++ react-redux-firebase/marioplan/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
@@ -8,6 +9,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     )
