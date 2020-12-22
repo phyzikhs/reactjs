21edf8dc9dfa8bb71af2e3009248f7a88e24e8fd
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-23 00:01:18 +0200

    BrowserRouter not BrouserRouter

--- react-redux-firebase/marioplan/src/App.js
+++ react-redux-firebase/marioplan/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import { BrouserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <BrouserRouter>
+      <BrowserRouter>
         <div className="App">
           <h1>Marioplan</h1>
         </div>
-      </BrouserRouter>
+      </BrowserRouter>
     )
   }
 }
