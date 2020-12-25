88039714917e649604241b16292a123fdc8b5f28
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-25 13:19:17 +0200

    adding auth from firebase as props on Navbar tracks auth status on which links to appear

--- react-redux-firebase/marioplan/src/components/layout/Navbar.js
+++ react-redux-firebase/marioplan/src/components/layout/Navbar.js
@@ -5,22 +5,24 @@ import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className='brand-logo'>Marioplan</Link>
       </div>
-      <SignedInLinks />
-      <SignedOutLinks />
+      { links }
     </nav>
   );
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
+  // console.log(state);
   return {
-
+    auth: state.firebase.auth
   }
 }
 
