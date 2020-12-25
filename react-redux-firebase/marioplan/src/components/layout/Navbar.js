5487b4294ac0af7471cca731fb95a39a474de46c
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-25 14:38:42 +0200

    Putting profile to the auth just add in useFirestoreForProfile: true pass in profile where you need it: navbar and signedinlinks

--- react-redux-firebase/marioplan/src/components/layout/Navbar.js
+++ react-redux-firebase/marioplan/src/components/layout/Navbar.js
@@ -6,9 +6,9 @@ import { connect } from 'react-redux'
 
 
 const Navbar = (props) => {
-  const { auth } = props;
-  console.log(auth);
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+  const { auth, profile } = props;
+  // console.log(props);
+  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
@@ -22,7 +22,8 @@ const Navbar = (props) => {
 const mapStateToProps = (state) => {
   // console.log(state);
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   }
 }
 
